8ab9735f3a80444266877490461f2164b378ef0f
esenakyol <esen.akyol.88@gmail.com>
2021-08-07 22:00:31 -0500

    ben geldim

--- src/saturday01/GroupStudy01.java
+++ src/saturday01/GroupStudy01.java
@@ -2,7 +2,8 @@ package saturday01;
 
 public class GroupStudy01 {
     public static void main(String[] args) {
+        //I changed some codes in GitHub!
+        System.out.println("Hey GitHub! What's the problem?");
 
-        System.out.println("Hey GitHub!");
     }
 }
