4cf0a672436118ab8fa0a4c02cc17493a416f1b0
esenakyol <88286989+esenakyol@users.noreply.github.com>
2021-08-07 21:57:01 -0500

    Update GroupStudy01.java

--- src/saturday01/GroupStudy01.java
+++ src/saturday01/GroupStudy01.java
@@ -4,5 +4,10 @@ public class GroupStudy01 {
     public static void main(String[] args) {
 
         System.out.println("Hey GitHub!");
+        System.out.println("Hey Java!");
+        System.out.println("Hey IntelliJ!");
+        System.out.println("Hey Eclipse!");
+        
+        
     }
 }
